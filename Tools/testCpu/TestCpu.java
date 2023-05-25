b85aa7573275d54d0d1100190b189bddac096b03
lsgf <zhuhaowen@hotwind.net>
2023-05-25 17:00:26 +0800

    update

--- Tools/testCpu/TestCpu.java
+++ Tools/testCpu/TestCpu.java
@@ -1,16 +1,24 @@
 public class TestCpu {
     public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
-        long endTime = startTime + 6000;
-        long index = 0;
-        while(true){
-            double x = Math.sqrt(index);
-            long now = System.currentTimeMillis();
-            if (now > endTime) {
-                break;
+        int times = 10;
+        long res = 0;
+        for (int i = 0; i < times; i++) {
+            long startTime = System.currentTimeMillis();
+            long endTime = startTime + 6000;
+            long index = 0;
+            while (true) {
+                double x = Math.sqrt(index);
+                long now = System.currentTimeMillis();
+                if (now > endTime) {
+                    break;
+                }
+                index++;
             }
-            index++;
+            System.out.println(index + " Loops in 6 seconds,test NO."+ i);
+            res+=index;
         }
-        System.out.println(index+" Loops in 6 seconds");
+        System.out.println(res/times + " avg Loops in 6 seconds,test "+ times +" times.");
     }
 }
+// company i5-10500 1811874294
+// my       desktop 984551935
